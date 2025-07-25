48ae48a6082c4626531d0a93cc501fa3d13bf58e
Ben Miles <benmilesGM@gmail.com>
2025-07-25 13:53:17 +0100

    adds products.html & product_detail.html

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,24 @@
+'''Products Admin.py'''
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    '''Admin class for Products'''
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'image'
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    '''Admin class for Categories'''
+    list_display = (
+        'friendly_name','name',        
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
