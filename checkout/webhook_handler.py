c65cc7bbe6cfd624654fbb4fc67986397e0aaa61
Ben Miles <benmilesGM@gmail.com>
2025-08-01 15:10:40 +0100

    adds webhook_handler.py to checkout app

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,18 @@
+'''Webhook Handler for Checkout app'''
+
+from django.http import HttpResponse
+
+
+class WebHookHandler:
+    """Handle Stripe webhooks"""
+
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        """
+        Handle a generic/unknown/unexpected webhook event
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
