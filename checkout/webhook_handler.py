5fe61bb9145af407f164876b7160415e8920ec56
Ben Miles <benmilesGM@gmail.com>
2025-08-01 15:17:52 +0100

    adds webhooks.py to checkout app

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -14,7 +14,7 @@ class WebHookHandler:
         Handle a generic/unknown/unexpected webhook event
         """
         return HttpResponse(
-            content=f'Webhook received: {event["type"]}',
+            content=f'Unhandled webhook received: {event["type"]}',
             status=200)
 
     def handle_payment_intent_succeeded(self, event):
