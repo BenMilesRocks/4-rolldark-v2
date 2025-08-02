69a8a9b7e15a04dc1235a8c6d578fecc1c013f78
Ben Miles <benmilesGM@gmail.com>
2025-08-02 19:01:02 +0100

    updates handle_payment_intent_succeeded to create orders from webhooks, webhook_handler.py

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,7 +1,13 @@
 '''Webhook Handler for Checkout app'''
+import json
+from time import sleep
 
 from django.http import HttpResponse
 
+import stripe
+
+from products.models import Product
+from .models import Order, OrderLineItem
 
 class WebHookHandler:
     """Handle Stripe webhooks"""
@@ -22,9 +28,102 @@ class WebHookHandler:
         Handle the payment_intent.succeeded webhook from Stripe
         """
         intent = event.data.object
-        print(intent)
+        pid = intent.id
+        cart = intent.metadata.cart
+        save_info = intent.metadata.save_info
+
+        # Get the Charge object
+        stripe_charge = stripe.Charge.retrieve(intent.latest_charge)
+
+        billing_details = stripe_charge.billing_details
+        shipping_details = intent.shipping
+        grand_total = round(stripe_charge.amount / 100, 2)
+
+        # Sets any blank values in Address to None
+        for field, value in shipping_details.address.items():
+            if value == "":
+                shipping_details.address[field] = None
+
+        order_exists = False # pylint: disable=W0612
+        attempt = 1
+
+        # Attempt to find order 5 times, allowing for slow server response
+        while attempt <= 5:
+
+            try:
+                # Check if Order exists
+                order = Order.objects.get( # pylint: disable=E1101
+                    full_name__iexact=shipping_details.name,
+                        email__iexact=shipping_details.email,
+                        phone_number__iexact=shipping_details.phone,
+                        country__iexact=shipping_details.country,
+                        postcode__iexact=shipping_details.postal_code,
+                        town_or_city__iexact=shipping_details.city,
+                        street_address1__iexact=shipping_details.line1,
+                        street_address2__iexact=shipping_details.line2,
+                        county__iexact=shipping_details.state,
+                    grand_total=grand_total,
+                )
+                # If it does, send message that Order is in DB
+                order_exists = True
+                break
+
+            except Order.DoesNotExist: # pylint: disable=E1101
+                # If order is not found, increment attempts
+                attempt += 1
+                sleep(1)
+
+        if order_exists:
+            return HttpResponse(
+                content=f'Webhook received: {event["type"]} | \
+                    SUCCESS: Verified order already in database',
+                status=200)
+        else:
+            # If Order does not exist, create it
+            order = None
+            try:
+                order = Order.objects.create( # pylint: disable=E1101
+                    # Create shipping and cost details
+                    full_name=shipping_details.name,
+                    email=shipping_details.email,
+                    phone_number=shipping_details.phone,
+                    country=shipping_details.country,
+                    postcode=shipping_details.postal_code,
+                    town_or_city=shipping_details.city,
+                    street_address1=shipping_details.line1,
+                    street_address2=shipping_details.line2,
+                    county=shipping_details.state,
+                    grand_total=grand_total,
+                )
+                # Add each item in order to Line Items
+                for item_id, item_data in json.loads(cart).items():
+                    product = Product.objects.get(id=item_id) # pylint: disable=E1101
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    else:
+                        for ticket_option, quantity in item_data['game_by_ticket_option'].items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                                ticket_option=ticket_option,
+                            )
+                            order_line_item.save()
+            except Exception as e: # pylint: disable=W0718
+                # All other errors, delete order and raise error
+                if order:
+                    order.delete()
+                return HttpResponse(
+                    content=f'Webhook received: {event["type"]} | ERROR: {e}',
+                    status=500)
+
         return HttpResponse(
-            content=f'Webhook received: {event["type"]}',
+            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
 
     def handle_payment_intent_payment_failed(self, event):
