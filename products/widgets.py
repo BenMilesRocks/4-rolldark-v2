c642e4605a714e9c2210f93fdb49a5774c6c787b
Ben Miles <benmilesGM@gmail.com>
2025-08-17 14:03:21 +0100

    adds widgets.py to products app

---
+++ products/widgets.py
@@ -0,0 +1,12 @@
+'''Products app Widgets.py'''
+
+from django.forms.widgets import ClearableFileInput
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomClearableFileInput(ClearableFileInput):
+    '''Creates custom ClearableFileInput, allowing field formatting'''
+    clear_checkbox_label = _('Remove')
+    initial_text = _('Current Image')
+    input_text = _('')
+    template_name = 'products/custom_widget_templates/custom_clearable_file_input.html'
