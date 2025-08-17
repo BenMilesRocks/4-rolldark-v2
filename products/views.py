3169fa6a7a104316e4d53580d352b6ce5d5dacd2
Ben Miles <benmilesGM@gmail.com>
2025-08-17 12:26:14 +0100

    adds POST handler to add_product(), products/views.py

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
 '''Products app views'''
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.contrib import messages
 
 from .models import Product
 from .forms import ProductForm
@@ -28,7 +30,17 @@ def product_detail(request, product_id):
 
 def add_product(request):
     '''Add a product to the store'''
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+
     template = 'products/add_product.html'
     context = {
         'form': form,
