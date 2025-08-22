4a36adf5275fca0206c3a3be719d5acd5d7d6b70
Ben Miles <benmilesGM@gmail.com>
2025-08-22 14:30:33 +0100

    fixes game_by_ticket_option in cart/contexts.py

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,6 +1,8 @@
 '''Cart app contexts'''
 from decimal import Decimal
 from django.shortcuts import get_object_or_404
+
+from rolldark.settings import DELIVERY_COST
 from products.models import Product
 
 def cart_contents(request):
@@ -12,17 +14,32 @@ def cart_contents(request):
     delivery = 0
     cart = request.session.get('cart', {})
 
-    for item_id, quantity in cart.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        cart_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in cart.items():
+
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            cart_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for option, quantity in item_data['game_by_ticket_option'].items():
+                total += quantity * product.price
+                product_count += quantity
+                cart_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'option': option,
+                })
+
         if product.delivery_charge:
-            delivery = Decimal(2.50) * Decimal(quantity)
+            delivery = Decimal(DELIVERY_COST) * Decimal(item_data)
 
     grand_total = float(delivery) + float(total)
 
