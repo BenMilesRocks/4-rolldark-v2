07864390ac95339f13d746fa589efcded8c22362
Ben Miles <benmilesGM@gmail.com>
2025-08-24 13:09:21 +0100

    adds call_to_action in context, home/views.py

--- home/views.py
+++ home/views.py
@@ -1,7 +1,15 @@
 """Home app views"""
 from django.shortcuts import render
 
+from .models import CallToAction
+
 def index(request):
     """View to return index page"""
 
-    return render(request, 'home/index.html')
+    call_to_action = CallToAction.objects.all() # pylint: disable=E1101
+
+    context = {
+        'call_to_action': call_to_action,
+    }
+
+    return render(request, 'home/index.html', context)
