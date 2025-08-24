3c606666b9caec091d5485c359f76a28ab161a22
Ben Miles <benmilesGM@gmail.com>
2025-08-24 12:28:03 +0100

    adds CallToAction() to home/models.py

--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
+'''Home App models'''
 from django.db import models
 
-# Create your models here.
+class CallToAction(models.Model):
+    '''Call to action panes for carousel on index.html'''
+
+    name = models.CharField(max_length=250)
+
+    headline = models.CharField(max_length=50, null=True, blank=True)
+
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    description = models.CharField(max_length=250)
+    dark_text = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.name)
