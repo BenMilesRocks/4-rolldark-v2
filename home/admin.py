8246e19ababe7ba645f521b7128895535c33b03f
Ben Miles <benmilesGM@gmail.com>
2025-08-24 12:58:45 +0100

    adds CallToActionAdmin() to home/admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,21 @@
+'''Home App admin.py'''
 from django.contrib import admin
 
-# Register your models here.
+from .models import CallToAction
+
+class CallToActionAdmin(admin.ModelAdmin):
+    '''Admin class for CallToAction'''
+
+    class Meta:
+        '''Fixes the pluralisation on the Admin page, shows Categories instead of Categorys'''
+        verbose_name_plural = 'Call To Action'
+
+    list_display = (
+        'name',
+        'headline',
+        'image',
+        'description',
+        'dark_text'
+    )
+
+admin.site.register(CallToAction, CallToActionAdmin)
