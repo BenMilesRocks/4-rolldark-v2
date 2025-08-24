4c290441e6b0fec50a29374d6139af21afb0574a
Ben Miles <benmilesGM@gmail.com>
2025-08-24 15:43:03 +0100

    adds add_call_to_action button on view_all_actions.html

--- home/urls.py
+++ home/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('add/', views.add_call_to_action, name='add_call_to_action'),
     path('view_all_actions/', views.view_all_actions, name='view_all_actions'),
     path('edit/<int:call_to_action_id>/', views.edit_call_to_action, name='edit_call_to_action'),
-    path('delete/<int:call_to_action_id>/', views.delete_call_to_action, name='delete_call_to_action'),
+    path('delete/<int:call_to_action_id>/',
+          views.delete_call_to_action, name='delete_call_to_action'),
 ]
