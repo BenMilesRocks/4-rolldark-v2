45ac93d2971b0edd1fb2b1bf77f3c70f10efbeb6
Ben Miles <benmilesGM@gmail.com>
2025-08-24 14:16:37 +0100

    adds add_call_to_action to home/views.py

--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
 """Home app views"""
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import CallToAction
+from .forms import CallToActionForm
 
 def index(request):
     """View to return index page"""
@@ -13,3 +17,29 @@ def index(request):
     }
 
     return render(request, 'home/index.html', context)
+
+@login_required
+def add_call_to_action(request):
+    '''Add a Call to Action to the home page'''
+
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = CallToActionForm(request.POST, request.FILES)
+        if form.is_valid():
+            call_to_action = form.save() # pylint: disable=W0612
+            messages.success(request, 'Successfully added Call to Action!')
+            return redirect(reverse('home'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = CallToActionForm()
+
+    template = 'home/add_call_to_action.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
