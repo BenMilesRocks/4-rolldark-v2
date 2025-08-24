efcd39668d81a457cf856d756587a8865f2c0901
Ben Miles <benmilesGM@gmail.com>
2025-08-24 15:21:11 +0100

    adds edit_call_to_action to home/urls.py

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('add/', views.add_call_to_action, name='add_call_to_action'),
     path('view_all_actions/', views.view_all_actions, name='view_all_actions'),
+    path('edit/<int:call_to_action_id>/', views.edit_call_to_action, name='edit_call_to_action'),
 ]
