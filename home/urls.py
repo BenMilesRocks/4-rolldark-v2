49e5ecd82d1527a58153c4f965befc7de85be341
Ben Miles <benmilesGM@gmail.com>
2025-08-24 14:55:18 +0100

    adds add_call_to_action to home/urls.py

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('add/', views.add_call_to_action, name='add_call_to_action'),
 ]
