031e03f283a73b23ae310b7bf93b65adb611569b
Ben Miles <benmilesGM@gmail.com>
2025-08-28 13:59:47 +0100

    fixes price display for campaign tickets in cart & checkout

--- cart/templatetags/cart_tools.py
+++ cart/templatetags/cart_tools.py
@@ -7,3 +7,20 @@ register = template.Library()
 def calc_subtotal(price, quantity):
     '''calculate subtotal for cart items'''
     return price * quantity
+
+@register.simple_tag(name='campaign_subtotal')
+def campaign_subtotal(price, dates, quantity):
+    '''calculate subtotal for campaign tickets'''
+    unit_price = float(price)
+    total_dates = len(dates)
+    number = float(quantity)
+    result = (unit_price * total_dates) * number
+    return f'{result:.2f}'
+
+@register.filter(name='campaign_cost')
+def campaign_cost(price, dates):
+    '''calculate price for campaign tickets'''
+    unit_price = float(price)
+    total_dates = len(dates)
+    result = unit_price * total_dates
+    return f'{result:.2f}'
