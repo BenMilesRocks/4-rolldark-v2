cdf224577c9df2b3c60805e50766d7b5a3b27ee5
Ben Miles <benmilesGM@gmail.com>
2025-08-30 21:14:38 +0100

    adds GameMaster to about/models.py

--- about/models.py
+++ about/models.py
@@ -1,3 +1,17 @@
+'''About app models'''
 from django.db import models
 
-# Create your models here.
+class GameMaster(models.Model):
+    '''Game Master profiles, allowing for dynamic creation of GM cards on about.html'''
+
+    name = models.CharField(max_length=250)
+
+    location = models.CharField(max_length=50, null=False, blank=False)
+
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    description = models.CharField(max_length=500, null=False, blank=False)
+
+    def __str__(self):
+        return str(self.name)
