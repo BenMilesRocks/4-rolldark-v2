663dc74885134a1cf897c032bc4ecec395b4dcb8
Ben Miles <benmilesGM@gmail.com>
2025-08-30 21:21:30 +0100

    adds about() to about/views.py

--- about/views.py
+++ about/views.py
@@ -1,3 +1,15 @@
+'''About app views'''
 from django.shortcuts import render
 
-# Create your views here.
+from .models import GameMaster
+
+def about(request):
+    """View to return about page"""
+
+    game_masters = GameMaster.objects.all() # pylint: disable=E1101
+
+    context = {
+        'game_masters': game_masters,
+    }
+
+    return render(request, 'about/about.html', context)
