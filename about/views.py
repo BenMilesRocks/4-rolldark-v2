b4d40ced52b803c20cbff4723b05f3ccbd2f081b
Ben Miles <benmilesGM@gmail.com>
2025-09-01 22:33:09 +0100

    adds edit_gm to about/views.py

--- about/views.py
+++ about/views.py
@@ -1,5 +1,5 @@
 '''About app views'''
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -40,6 +40,35 @@ def add_gm(request):
     template = 'about/add_gm.html'
     context = {
         'form': form,
+    }
+
+    return render(request, template, context)
+
+@login_required
+def edit_gm(request, gm_id):
+    '''Edit a call to action in the carousel'''
+
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    gm = get_object_or_404(GameMaster, pk=gm_id)
+    if request.method == 'POST':
+        form = GameMasterForm(request.POST, request.FILES, instance=gm)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('home'))
+        else:
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+    else:
+        form = GameMasterForm(instance=gm)
+        messages.info(request, f"You are editing {gm.name}'s profile")
+
+    template = 'about/edit_gm.html'
+    context = {
+        'form': form,
+        'gm': gm,
     }
 
     return render(request, template, context)
