2524cb7e46444d673dd9aeb73bfadb486d24f148
Ben Miles <benmilesGM@gmail.com>
2025-09-01 22:34:55 +0100

    adds edit_gm to about/urls.py

--- about/urls.py
+++ about/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.about, name='about'),
     path('add_gm/', views.add_gm, name='add_gm'),
+    path('edit_gm/<int:gm_id>/', views.edit_gm, name='edit_gm'),
 ]
