633fec56480cdf9be69b32879ee6addeaf332e62
Ben Miles <benmilesGM@gmail.com>
2025-09-01 20:31:54 +0100

    adds add_gm to about/views.py

--- about/views.py
+++ about/views.py
@@ -1,7 +1,11 @@
 '''About app views'''
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import GameMaster
+from .forms import GameMasterForm
 
 def about(request):
     """View to return about page"""
@@ -13,3 +17,29 @@ def about(request):
     }
 
     return render(request, 'about/about.html', context)
+
+@login_required
+def add_gm(request):
+    '''Add a Call to Action to the home page'''
+
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = GameMasterForm(request.POST, request.FILES)
+        if form.is_valid():
+            game_master = form.save() # pylint: disable=W0612
+            messages.success(request, f'Successfully added { game_master.name } to Game Masters!')
+            return redirect(reverse('home'))
+        else:
+            messages.error(request, 'Failed to add Game Master. Please ensure the form is valid.')
+    else:
+        form = GameMasterForm()
+
+    template = 'home/add_call_to_action.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
